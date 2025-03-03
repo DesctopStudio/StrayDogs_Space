ac789cc34a2a13a8d50d44dc9c0c3736e2690afb
SummingSines <contact@desctop.studio>
2025-03-02 23:39:51 -0800

    Initial commit includes the home, about and app.py files tied together via a flask project in Pycharm.

--- app.py
+++ app.py
@@ -5,11 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
-
-@app.route("/about_us")
-def about_us():
-    return render_template("about_us.html")
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run()
